f79b9db223878df9b3bd4b33bdad5adaf70a0ba4
duyenN01 <duyen.t.nguyen01@sjsu.edu>
2021-11-15 20:05:37 -0800

    adding my files

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+let array =[];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600,600);
+  background(220, 50, 130);
+  strokeWeight(5);
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed == true) {
+    //stroke(map(mouseX,0,600,0,255,true),map(mouseY,0,600,0,115,true) );
+    // line(width- mouseX, height-mouseY, width-pmouseX, height-pmouseY);
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+  }
+
+}
+function keyTyped(){
+
+  if (key === 's'){
+    //save this image
+    saveCanvas('fileName', 'png');
+
+  } else if (key === 'd'){
+    //display image
+    //navigating x n y
+    // console.log(array [0]);
+    // console.log(array [0][1]);
+    for (let i = 0; i<array.length, i++){
+
+
+    line(array[i][0,array[i][1],array[i+1][0],array[i+1][1]);
+  }
+}
+  return false;
+
 }
